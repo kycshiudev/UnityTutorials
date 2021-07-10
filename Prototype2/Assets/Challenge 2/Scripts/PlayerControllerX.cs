1a21d25a0466837e30017d6fdba1cbfb3b7bb965
Ken S <kycshiudev@gmail.com>
2021-07-10 12:26:43 -0700

    Through Challenge 2

---
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogCooldownDuration = 1.0f;
+
+    private bool dogLoaded = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogLoaded)
+        {
+            dogLoaded = false;
+            Invoke("ReloadDog", dogCooldownDuration);
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    void ReloadDog()
+    {
+        dogLoaded = true;
+    }
+}
