234dc8af4aaf3817eec50d9091444f0efa5eb468
Ken S <kycshiudev@gmail.com>
2021-07-10 11:00:52 -0700

    Through Lesson 2.4

--- Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,9 +16,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound || transform.position.z < lowerBound)
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+            Debug.Log("Game Over");
         }
     }
 }
--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,23 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animals;
     private int spawnPosZ = 20;
     private int spawnRangeX = 20;
+    public float spawnRate = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", 2, spawnRate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            var index = Random.Range(0, animals.Length);
-            var selectedAnimal = animals[index];
-            Instantiate(selectedAnimal, new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ), selectedAnimal.transform.rotation);
-        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        var index = Random.Range(0, animals.Length);
+        var selectedAnimal = animals[index];
+        Instantiate(selectedAnimal, new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ), selectedAnimal.transform.rotation);
     }
 }
