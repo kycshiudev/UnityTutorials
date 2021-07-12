b84982f393495773bbfe59d107d396dd0aaf0ec0
Ken S <kycshiudev@gmail.com>
2021-07-12 16:44:31 -0700

    Through Lesson 3.2

--- Prototype3/Assets/Scripts/MoveLeft.cs
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 15.0f;
+    private float bottomBound = -10.0f;
+    private PlayerController player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (!player.gameOver)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (transform.position.y < bottomBound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Prototype3/Assets/Scripts/SpawnManager.cs
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(35, 0, 0);
+    private Vector3 spawnPos = new Vector3(35, 0, 2);
     public float startDelay = 2.0f;
     public float repeatRate = 3.0f;
+    private PlayerController player;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -23,6 +25,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (!player.gameOver)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
